0bf3d6e4598214ff4aed8a0dc83b8a2a053403fc
Nghi Hua <huanhatgianghi@gmail.com>
2022-08-17 02:26:37 +0700

    add cat token with left-right movements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [xCoordinate, setXCoordinate] = useState<number>(0);
+  const [isFacedLeft, setIsFacedLeft] = useState<boolean>(true);
+  const windowWidth = window.innerWidth;
+  const catSize = 300;
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log(event);
+    if (event.code==="ArrowLeft" && xCoordinate>0) {
+      setXCoordinate(xCoordinate - 20);
+      setIsFacedLeft(true);
+    }
+    if (event.code==="ArrowRight" && ((xCoordinate + catSize)<windowWidth)) {
+      setXCoordinate(xCoordinate + 20);
+      setIsFacedLeft(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      onKeyDown={handleKeyDown}
+      tabIndex={0}
+      className='bg-gray-200 w-screen h-screen'>
+      <div className={`absolute`}
+        style={{
+          left: `${xCoordinate}px`,
+          width: `${catSize}px`
+        }}
+      >
+        <img className={`${isFacedLeft? "" : "flipped"}`} src='cat.png'/>
+      </div>
     </div>
   );
 }
